58de9460c0fbd3606974408ae2688db28fd4f85f
Thrymst <115964158+Thrymst@users.noreply.github.com>
2023-02-24 10:57:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-let count = 0
-let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById('save-el')
+let count = 0
 
-function increment() {
-    count += 1
+function increament(){
+    count = count + 1
     countEl.textContent = count
 }
 
-function save() {
-    let countStr = count + " - "
-    saveEl.textContenttStr
+function save(){
+  let  countstr = count + ' - '
+    saveEl.textContent += countstr
     countEl.textContent = 0
     count = 0
-}
 
-console.log("Let's count people on the subway!")
+}
